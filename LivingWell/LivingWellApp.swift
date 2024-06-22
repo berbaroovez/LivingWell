93fafb3d88045ba30a2588fc8fab0adba9399a6d
EphraimJZimmerman <68978246+EphraimJZimmerman@users.noreply.github.com>
2024-06-22 14:22:54 -0600

    Initial Commit

---
+++ LivingWell/LivingWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivingWellApp.swift
+//  LivingWell
+//
+//  Created by Ephraim Zimmerman on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LivingWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
